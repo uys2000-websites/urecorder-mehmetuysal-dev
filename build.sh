4d2d8f085bfda65080553fa6512814538bca55f3
Mehmet Uysal <siliente2020@gmail.com>
2025-02-04 18:49:52 +0300

    update

---
+++ build.sh
@@ -0,0 +1,13 @@
+## set NodeJs and Yarn
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+## set deploy path
+export DEPLOYPATH=/home/mehmetu/uplayer-mehmetuysal-dev/
+
+yarn
+yarn build
+rm -rf $DEPLOYPATH/*
+/bin/cp -R dist/. $DEPLOYPATH
+/bin/cp -R dist/.htaccess $DEPLOYPATH/.htaccess
