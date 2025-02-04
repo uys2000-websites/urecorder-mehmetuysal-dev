d30b4d4d5a6a7e5c052cdcac4fd6c9c1d6777386
Mehmet Uysal <siliente2020@gmail.com>
2025-02-04 18:53:54 +0300

    update

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 ## set deploy path
-export DEPLOYPATH=/home/mehmetu/uplayer-mehmetuysal-dev/
+export DEPLOYPATH=/home/mehmetu/urecorder-mehmetuysal-dev/
 
 yarn
 yarn build
